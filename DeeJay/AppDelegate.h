219112b09019995948c06a61b6ed8f65f0f70576
Mike Berlin <mikeberlin@me.com>
2013-05-08 19:11:33 -0500

    Initial Commit

---
+++ DeeJay/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  DeeJay
+//
+//  Created by Mike Berlin on 5/8/13.
+//  Copyright (c) 2013 Mike Berlin. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ DeeJay/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  DeeJay
+//
+//  Created by Mike Berlin on 5/8/13.
+//  Copyright (c) 2013 Mike Berlin. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char *argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
